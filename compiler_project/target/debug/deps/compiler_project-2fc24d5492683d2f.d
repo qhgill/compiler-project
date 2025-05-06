48271a5b174f97bc886c1442d903bc43b84e265a
Kolzza <yoshi88639@gmail.com>
2025-05-06 01:22:33 +0000

    finished(?)

--- compiler_project/target/debug/deps/compiler_project-2fc24d5492683d2f.d
+++ compiler_project/target/debug/deps/compiler_project-2fc24d5492683d2f.d
@@ -1,5 +1,5 @@
-/home/csmajs/qgill001/teh-tarik-compiler-qgill-and-cmill/compiler_project/target/debug/deps/compiler_project-2fc24d5492683d2f: src/main.rs
+/home/csmajs/cmill100/teh-tarik-compiler-qgill-and-cmill/compiler_project/target/debug/deps/compiler_project-2fc24d5492683d2f: src/main.rs
 
-/home/csmajs/qgill001/teh-tarik-compiler-qgill-and-cmill/compiler_project/target/debug/deps/compiler_project-2fc24d5492683d2f.d: src/main.rs
+/home/csmajs/cmill100/teh-tarik-compiler-qgill-and-cmill/compiler_project/target/debug/deps/compiler_project-2fc24d5492683d2f.d: src/main.rs
 
 src/main.rs:
